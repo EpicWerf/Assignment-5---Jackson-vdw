a6eee7081f4dd864b02e29669ef2c7ddef504fd8
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-03-03 08:06:50 -0700

    Finished everything for Assignement 7

--- Models/ViewModels/BookListViewModel.cs
+++ Models/ViewModels/BookListViewModel.cs
@@ -10,7 +10,13 @@ namespace Assignment_5___Jackson_vdw.Models.ViewModels
 {
     public class BookListViewModel
     {
+        // The IEnumberable is only pulled in the index page with the following statement
+        // "@foreach (var x in Model.Projects)"
         public IEnumerable<Book> Books { get; set; }
+
+        //this one is used to set how many pages there are
         public PagingInfo PagingInfo { get; set; }
+        //next line will let you access the category from the controller
+        public string Category { get; set; }
     }
 }
